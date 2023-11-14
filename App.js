d95c45e3a2975ea305e67c28f37ee429d9052bfd
ChrisWhisker <chrisworc@gmail.com>
2023-11-13 22:23:57 -0500

    Update choices when correct answer chosen

--- App.js
+++ App.js
@@ -4,33 +4,49 @@ import ChoiceButton from './components/ChoiceButton';
 import FlashCard from './components/FlashCard';
 
 const App = () => {
-  // Variables
-  const [factor1, setfactor1] = useState(Math.floor(Math.random() * 10) + 1);
-  const [factor2, setfactor2] = useState(Math.floor(Math.random() * 10) + 1);
-  const solution = factor1 * factor2;
+  // Functions
+  const makeFactor = () => {
+    return Math.floor(Math.random() * 10) + 1;
+  }
 
-  const [choices, setChoices] = useState([
-    (factor1 + 1) * factor2,
-    solution,
-    Math.floor(Math.random() * 100) + 1,
-    factor1 * (factor2 - 1),
-  ]);
+  const makeChoices = (num1, num2) => {
+    return [
+      (num1 + 1) * num2,
+      num1 * num2,
+      Math.floor(Math.random() * 100) + 1,
+      num1 * (num2 - 1),
+    ]
+  }
 
   const pressButton = (choice) => {
+    console.log(`Solution is ${solution}. Button ${choice} was pressed.`)
     if (choice === solution) {
-        alert(`You're right!`);
+      alert(`You're right!`);
+      const newFactor1 = makeFactor();
+      setfactor1(newFactor1);
+      const newFactor2 = makeFactor();
+      setfactor2(newFactor2);
+      const newSolution = newFactor1 * newFactor2;
+      setsolution(newSolution);
+      setChoices(makeChoices(newFactor1, newFactor2));
     } else {
-        alert('Incorrect.');
+      alert('Try again.');
     }
   }
+
+  // Variables
+  const [factor1, setfactor1] = useState(makeFactor());
+  const [factor2, setfactor2] = useState(makeFactor());
+  const [solution, setsolution] = useState(factor1 * factor2);
+  const [choices, setChoices] = useState(makeChoices(factor1, factor2));
 
   // App
   return (
     <View flexDirection={'column'} flex={1}>
       <View style={styles.questionContainer}>
-        <FlashCard question={`${factor1} x ${factor2}`}/>
+        <FlashCard question={`${factor1} x ${factor2}`} />
       </View>
-      <View style={styles.choicesContainter}>
+      <View style={styles.choicesContainer}>
         {choices.map((choice) => (
           <ChoiceButton choice={choice} pressHandler={pressButton} key={choice}></ChoiceButton>
         ))}
@@ -45,7 +61,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#2c313c',
     justifyContent: 'center',
   },
-  choicesContainter: {
+  choicesContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-evenly',
