730b9a4f1ccd0db5358f7b466977d3f447254f06
ChrisWhisker <chrisworc@gmail.com>
2023-11-16 08:56:38 -0500

    Generate wrong answers intelligently

--- App.tsx
+++ App.tsx
@@ -9,15 +9,59 @@ const App = () => {
     return Math.floor(Math.random() * 10) + 1;
   };
 
+  const generateWrongAnswer = (
+    f1: number,
+    f2: number,
+    choiceList: number[]
+  ) => {
+    let wrongAnswer: number, approachName: string, details = "";
+    const generationApproach = Math.random();
+
+    if (generationApproach < 0.667) {
+      // increment or decrement one of the factors
+      approachName = "Shifted Factor";
+      do {
+        const factorToShift = Math.random() > 0.5 ? f1 : f2;
+        const shiftAmount = Math.random() > 0.5 ? 1 : -1;
+
+        if (factorToShift === f1) {
+          wrongAnswer = (factorToShift + shiftAmount) * f2;
+          details = `(${(factorToShift + shiftAmount)} x ${f2})`;
+        } else {
+          wrongAnswer = f1 * (factorToShift + shiftAmount);
+          details = `(${f1} x ${(factorToShift + shiftAmount)})`;
+        }
+      } while (choiceList.includes(wrongAnswer));
+    } else if (generationApproach < 0.889) {
+      // new equation
+      approachName = "New Equation";
+      const newFactor1 = Math.floor(Math.random() * 10) + 1;
+      do {
+        const newFactor2 = Math.floor(Math.random() * 10) + 1;
+        wrongAnswer = newFactor1 * newFactor2;
+        details = `(${newFactor1} x ${newFactor2})`;
+      } while (choiceList.includes(wrongAnswer));
+    } else {
+      // random 1-100
+      approachName = "Random Number 1-100";
+      do {
+        wrongAnswer = Math.floor(Math.random() * 100) + 1;
+      } while (choiceList.includes(wrongAnswer));
+    }
+    console.log(
+      `Choice ${wrongAnswer} generated using approach: ${approachName} ${details}`
+    );
+    return wrongAnswer;
+  };
+
   const makeChoices = (f1: number, f2: number): number[] => {
     console.log(`Making choices. Equation is ${f1} x ${f2}`);
-    const newChoices = [
-      f1 * f2,
-      (f1 + 1) * f2,
-      f1 * (f2 - 1),
-      Math.floor(Math.random() * 100) + 1,
-    ];
-    console.log(`New choices are ${newChoices}`);
+    let newChoices = [f1 * f2];
+
+    for (let i = 0; i < 3; i++) {
+      newChoices.push(generateWrongAnswer(f1, f2, newChoices));
+    }
+    // console.log(`New choices are ${newChoices}`);
     return newChoices;
   };
 
@@ -29,14 +73,18 @@ const App = () => {
     const newSolution = newFactor1 * newFactor2;
     setSolution(newSolution);
     // Randomize order of choices
-    const newChoices = makeChoices(newFactor1, newFactor2).sort(() => Math.random() - 0.5);
+    const newChoices = makeChoices(newFactor1, newFactor2).sort(
+      () => Math.random() - 0.5
+    );
     console.log(`New choices shuffled are ${newChoices}`);
     setChoices(newChoices);
   };
 
   const pressButton = (choice: number) => {
     if (choice === solution) {
-      Alert.alert("You're right!", "", [{ text: "New problem", onPress: makeNewEquation }]);
+      Alert.alert("You're right!", "", [
+        { text: "New problem", onPress: makeNewEquation },
+      ]);
     } else {
       Alert.alert("That's wrong.", "", [{ text: "Try again" }]);
     }
@@ -49,7 +97,7 @@ const App = () => {
   const [choices, setChoices] = useState(() => makeChoices(factor1, factor2));
 
   // App
-    return (
+  return (
     <View style={styles.root}>
       <View style={styles.questionContainer}>
         <FlashCard question={`${factor1} x ${factor2}`} />
